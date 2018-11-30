f2c3881600491eed25715f6680a1e1923408280c
Marcus Kinsella <mkinsella@chanzuckerberg.com>
2018-11-30 17:40:55 +0000

    Assume python3 virtualenv in deploy

--- service/deploy.sh
+++ service/deploy.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Zip up the lambda deployment package
 rm -f portability-service-lambda.zip
-pip3 install --system -t portability-service-lambda/ requests
+pip install -U -t portability-service-lambda/ requests
 cd portability-service-lambda && zip -X -r ../portability-service-lambda.zip * && cd ..
 aws s3 cp portability-service-lambda.zip s3://"$2"/portability-service-lambda.zip
 
